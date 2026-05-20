e373e374cba8e4b0bbe492ec7666243a69099cbc
iisaacsr <isaacsroy3@gmail.com>
2026-05-19 22:18:14 -0400

    basic hyprland config

--- hyprland.nix
+++ hyprland.nix
@@ -11,7 +11,21 @@
       };
 
       monitor = [
-        ",preferred,auto,auto"
+        ",preferred,auto,1"
+      ];
+
+      "$mod" = "SUPER";
+      bind = [
+        # arrow key move thru windows
+        "$mod, left, movefocus, l"
+        "$mod, right, movefocus, r"
+        "$mod, up, movefocus, u"
+        "$mod, down, movefocus, d"
+        # workspace moving
+        "$mod SHIFT, 1, movetoworkspace, 1"
+        "$mod SHIFT, 2, movetoworkspace, 2"
+        "$mod SHIFT, 3, movetoworkspace, 3"
+        "$mod SHIFT, 4, movetoworkspace, 4"
       ];
     };
   };
