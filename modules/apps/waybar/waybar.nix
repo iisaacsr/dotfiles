20f7f559b042d97dd44b8629d861ac00ea375cfa
iisaacsr <isaacsroy3@gmail.com>
2026-05-23 18:58:04 -0400

    waybar config

--- modules/apps/waybar/waybar.nix
+++ modules/apps/waybar/waybar.nix
@@ -7,18 +7,77 @@
       mainBar = {
         layer = "top";
         position = "top";
-        height = 40;
+        height = 30;
         spacing = 4;
-        modules-left = [ "hyprland/workspaces" ];
-        modules-center = [ "clock" ];
+        modules-left = [
+          "hyprland/workspaces"
+          "custom/sep"
+          "hyprland/window"
+          "custom/sep"
+        ];
         modules-right = [
+          "custom/sep"
+          "network"
+          "custom/sep"
           "cpu"
+          "custom/sep"
           "memory"
-          "battery"
+          "custom/sep"
+          "disk"
+          "custom/sep"
+          "clock"
+          "custom/sep"
           "tray"
         ];
         "hyprland/workspaces" = {
+          disable-scroll = true;
+          all-outputs = true;
+          warp-on-scroll = false;
           format = "{name}";
+          persistent-workspaces = 5;
+        };
+        "hyprland/window" = {
+          max-length = 40;
+          seperate-outputs = false;
+        };
+        "tray" = {
+          spacing = 10;
+        };
+        "clock" = {
+          format-alt = "{:%Y-%m-%d}";
+        };
+        "memory" = {
+          format = "Mem: {used}G";
+        };
+        "disk" = {
+          interval = 60;
+          format = "Disk: {free}";
+        };
+        "battery" = {
+          states = {
+            "good" = 95;
+            "warning" = 30;
+            "critical" = 15;
+          };
+          format = "Bat: {capacity}% {icon} {time}";
+          format-plugged = "{capacity}% ";
+          format-alt = "Bat {capacity}%";
+          format-time = "{H}:{M}";
+          format-icons = [
+            ""
+            ""
+            ""
+            ""
+            ""
+          ];
+        };
+        "network" = {
+          format = "Online";
+          format-disconnected = "Disconnected ⚠";
+        };
+        "custom/sep" = {
+          format = "|";
+          interval = 0;
         };
       };
     };
