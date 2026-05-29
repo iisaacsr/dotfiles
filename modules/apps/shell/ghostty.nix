96d7bc78543f095c8a30f7e10e0e1ceee404e9cb
iisaacsr <isaacsroy3@gmail.com>
2026-05-28 21:27:33 -0400

    add direnv

--- modules/apps/shell/ghostty.nix
+++ modules/apps/shell/ghostty.nix
@@ -2,11 +2,17 @@
 {
   programs.ghostty = {
     enable = true;
+    enableBashIntegration = true;
 
     settings = {
       theme = "Black Metal (Bathory)";
       font-size = 11;
       background-opacity = 0.9;
     };
+  };
+
+  programs.direnv = {
+    enable = true;
+    nix-direnv.enable = true;
   };
 }
